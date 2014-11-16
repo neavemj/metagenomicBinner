a165716976080062fe5081eddf02c15133ff2ebc
neavemj <matthewjneave1@gmail.com>
2014-11-16 19:20:34 +0300

    Initial commit

---
+++ pyqtgraphTest.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+"""
+Matt's metagenomic binner using pyqtgraph
+Graphical interface for selecting contigs and checking genome completeness
+
+"""
+
+# imports
+
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+import numpy as np
+from pyqtgraph.dockarea import *
+
+## add gui
+
+app = QtGui.QApplication([])
+window = QtGui.QMainWindow()
+window.resize(800, 800)
+view = pg.GraphicsLayoutWidget()
+window.setCentralWidget(view)
+window.show()
+window.setWindowTitle('metagenomicBinner')
+
+## create areas for graph and stats
+
+window1 = view.addPlot()
+window2 = view.addViewBox()
+
+print("Generating data, this takes a few seconds...")
+
+# data for plots
+
+mattsArray = np.arange(300)
+#mattsArray.shape = (2,150)
+
+scatter1 = pg.ScatterPlotItem()
+scatter1.addPoints(mattsArray, mattsArray)
+window1.addItem(scatter1)
+
+genomeStats = pg.TextItem(text='you have selected %s points' % 0)
+#genomeStats.setPos(40,40)
+window2.addItem(genomeStats)
+
+# add region of interest rectangle
+
+roi = pg.RectROI(pos=[10, 10], size=[20, 20])
+window1.addItem(roi)
+
+# get points within ROI
+# first define a function to pull out points from the scatter array
+# function will be called when selection with ROI is finished
+
+
+def matts_function():
+    pointCount = 0
+    x_min_bound = roi.pos()[0]
+    y_min_bound = roi.pos()[1]
+    x_max_bound = x_min_bound + roi.size()[0]
+    y_max_bound = y_min_bound + roi.size()[1]
+    ## check which points are within these bounds
+    for pts in mattsArray:
+        if pts > x_min_bound and pts < x_max_bound and pts > y_min_bound and pts < y_max_bound:
+            pointCount += 1
+    print 'total points selected:', pointCount
+    genomeStats.setText('you have selected %s points' % pointCount)
+
+# this is a 'signal' in pyqtgraph used to call the function
+
+roi.sigRegionChangeFinished.connect(matts_function)
+
+
+# Start Qt event loop unless running in interactive mode.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
