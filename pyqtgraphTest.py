75cd4f706e17106a4cd3d114a9bd9206658f9cf0
neavemj <matthewjneave1@gmail.com>
2014-12-04 12:04:02 +0300

    Added statistics window

--- pyqtgraphTest.py
+++ pyqtgraphTest.py
@@ -33,53 +33,76 @@ window2 = view.addLabel("You have not selected any points")
 
 # data for plots
 
-# AHtCov = open("end11AHt.coverage.csv")
-# AHphCov = open("end43AHpf.coverage.csv")
+AHtCov = open("end11AHt.coverage.csv")
+AHphCov = open("end43AHpf.coverage.csv")
+
+tDict = {}
+phDict = {}
+
+
+#TODO: make numpy array containing scatterplot points
+#TODO: subsample data for display
+
+# create dictionaries for the coverage values. Key is scaffold name, value is coverage.
+
+for line in AHtCov:
+    try:
+        cols = line.split(',')
+        number = float(cols[1])
+        tDict[cols[0]] = number
+    except:
+        continue
+
+for line in AHphCov:
+    try:
+        cols = line.split(',')
+        number = float(cols[1])
+        phDict[cols[0]] = number
+    except:
+        continue
+
+# put dictionaries together into an array for plotting, ensuring coverage is for the correct scaffold
+# the dictionaries are of unequal length because some scaffs have 0 coverage (and no row) for a particular treatment
+
+#TODO: somehow need to get complete set of scaffolds that are in the dictionaries..
+
+
+
 #
-# mattsArray = []
+# #mattsArray = np.arange(300)
 #
-# for line in AHtCov:
-#     try:
-#         cols = line.split(',')
-#         number = float(cols[1])
-#         mattsArray.append(cols[1])
-#     except:
-#         continue
-
-mattsArray = np.arange(300)
-
-scatter1 = pg.ScatterPlotItem()
-scatter1.addPoints(mattsArray, mattsArray)
-window1.addItem(scatter1)
-
-# add region of interest rectangle
-
-roi = pg.RectROI(pos=[10, 10], size=[20, 20])
-roi.addScaleHandle(0,1)
-window1.addItem(roi)
-
-# get points within ROI
-# first define a function to pull out points from the scatter array
-# function will be called when selection with ROI is finished
-
-def matts_function():
-    pointCount = 0
-    x_min_bound = roi.pos()[0]
-    y_min_bound = roi.pos()[1]
-    x_max_bound = x_min_bound + roi.size()[0]
-    y_max_bound = y_min_bound + roi.size()[1]
-    ## check which points are within these bounds
-    for pts in mattsArray:
-        if pts > x_min_bound and pts < x_max_bound and pts > y_min_bound and pts < y_max_bound:
-            pointCount += 1
-    if pts > 0:
-        print 'total points selected:', pointCount
-        window2.setText('you have selected %s points' % pointCount)
-
-# this is a 'signal' in pyqtgraph used to call the function
-
-roi.sigRegionChangeFinished.connect(matts_function)
-
+# scatter1 = pg.ScatterPlotItem()
+# scatter1.addPoints(mattsArray, mattsArray)
+# window1.addItem(scatter1)
+#
+# # add region of interest rectangle
+#
+# roi = pg.RectROI(pos=[10, 10], size=[20, 20])
+# roi.addScaleHandle(0,1)
+# window1.addItem(roi)
+#
+# # get points within ROI
+# # first define a function to pull out points from the scatter array
+# # function will be called when selection with ROI is finished
+#
+# def matts_function():
+#     pointCount = 0
+#     x_min_bound = roi.pos()[0]
+#     y_min_bound = roi.pos()[1]
+#     x_max_bound = x_min_bound + roi.size()[0]
+#     y_max_bound = y_min_bound + roi.size()[1]
+#     ## check which points are within these bounds
+#     for pts in mattsArray:
+#         if pts > x_min_bound and pts < x_max_bound and pts > y_min_bound and pts < y_max_bound:
+#             pointCount += 1
+#     if pts > 0:
+#         print 'total points selected:', pointCount
+#         window2.setText('you have selected %s points' % pointCount)
+#
+# # this is a 'signal' in pyqtgraph used to call the function
+#
+# roi.sigRegionChangeFinished.connect(matts_function)
+#
 
 # Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
