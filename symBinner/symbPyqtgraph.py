9c1ce6dd0e6597f0c85ee8b737639468a0f110e8
neavemj <matthewjneave1@gmail.com>
2015-02-02 17:58:11 +0300

    fixed widget setup

--- symBinner/symbPyqtgraph.py
+++ symBinner/symbPyqtgraph.py
@@ -12,25 +12,8 @@ Graphical interface module for selecting contigs and checking genome completenes
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
-import symbData, symbHelper
+import symbData, symbHelper, symbROIselector
 
-## add gui
-
-#TODO: add a zoom slider
-
-app = QtGui.QApplication([])
-window = QtGui.QMainWindow()
-window.resize(1000, 800)
-view = pg.GraphicsLayoutWidget()
-window.setCentralWidget(view)
-window.show()
-window.setWindowTitle('metagenomicBinner')
-
-# ## create area for graph and add a label to display the statistics
-
-window1 = view.addPlot()
-window1.setClipToView(clip=True)
-window1.setLabels(left='coverage 1 (log scale)', bottom='coverage 2 (log scale)')
 
 # get data and import using symbData.py module
 
@@ -62,7 +45,6 @@ print 'gc range:', minGCcontent, avgGCcontent, maxGCcontent
 color = np.array([[231,246,189],[102,6,95],[229,82,7],[74,94,20],[249,149,161],[38,241,240],[103,20,27],[253,203,120],[250,77,137],[248,55,66]], dtype=np.ubyte)
 colmap = pg.ColorMap(point, color)
 
-
 # use list comprehension to add my data points to a list of dictionaries as required by pyqtgraph
 # divided length by 500 because this looked about right but should come up with something better
 # spots to draw is those greater than 1000 (after dividing by 500 in previous step)
@@ -80,48 +62,84 @@ print 'selected %d points larger than %d bps to draw' % (len(spots_to_draw), siz
 
 print "*** Adding Spots to Window ***"
 
-scatter1 = pg.ScatterPlotItem()
-scatter1.addPoints(spots=spots_to_draw)
-window1.addItem(scatter1)
+## add gui
 
-# add region of interest rectangle
+#TODO: add a zoom slider
 
-roi = pg.RectROI(pos=[0, 0], size=[2, 2])
-roi.addScaleHandle(0,1)
-window1.addItem(roi)
+app = QtGui.QApplication([])
+window = QtGui.QMainWindow()
+window.resize(1000, 800)
+window.show()
+window.setWindowTitle('metagenomicBinner')
 
-window2 = view.addLabel("Welcome to symbBinner")
-contig_length_view = view.addLabel()
+#view = pg.GraphicsLayoutWidget()
+cw = QtGui.QWidget()
+window.setCentralWidget(cw)
 
-# get points within ROI
-# first define a function to pull out points from the scatter array
-# function will be called when selection with ROI is finished
+# ## create area for graph and add a label to display the statistics
+##???
+l = QtGui.QGridLayout()
+cw.setLayout(l)
+l.setSpacing(0)
 
-#TODO: create module to estimate genome coverage, etc.
+v = pg.GraphicsView()
+vb = pg.ViewBox()
+vb.setAspectLocked()
+v.setCentralItem(vb)
+l.addWidget(v, 0, 0)
 
-def roiSelector():
-    pointCount = 0
-    point_combined_length = 0
-    x_min_bound = roi.pos()[0]
-    y_min_bound = roi.pos()[1]
-    x_max_bound = x_min_bound + roi.size()[0]
-    y_max_bound = y_min_bound + roi.size()[1]
-    ## check which points are within these bounds
-    for pts in spots:
-        if pts['pos'][0] > x_min_bound and pts['pos'][0] < x_max_bound and pts['pos'][1] > y_min_bound and pts['pos'][1] < y_max_bound:
-            pointCount += 1
-            point_combined_length += pts['size']
-    if pts > 0:
-        print 'total points selected:', pointCount
-        print 'length of contigs (bps):', int(point_combined_length)
-        window2.setText('you have selected %s points' % pointCount)
-        contig_length_view.setText('length of contigs (bps):')
+plot = pg.PlotWidget()
 
-#TODO: make use of sigClicked? To give info about specific point?
+w = pg.ScatterPlotItem()
+w.addPoints(spots=spots_to_draw)
 
-# this is a 'signal' in pyqtgraph used to call the function
+plot.addItem(w)
 
-roi.sigRegionChangeFinished.connect(roiSelector)
+l.addWidget(plot, 0, 1)
+
+#window1.setClipToView(clip=True)
+#window1.setLabels(left='coverage 1 (log scale)', bottom='coverage 2 (log scale)')
+
+#scatter1 = pg.ScatterPlotItem()
+#scatter1.addPoints(spots=spots_to_draw)
+#v.addItem(scatter1)
+
+# # add region of interest rectangle
+#
+# roi = pg.RectROI(pos=[0, 0], size=[2, 2])
+# roi.addScaleHandle(0,1)
+# window1.addItem(roi)
+#
+# window2 = view.addLabel("Welcome to symbBinner")
+# contig_length_view = view.addLabel()
+#
+# # get points within ROI
+# # first define a function to pull out points from the scatter array
+# # function will be called when selection with ROI is finished
+#
+# def roiSelector():
+#     pointCount = 0
+#     point_combined_length = 0
+#     x_min_bound = roi.pos()[0]
+#     y_min_bound = roi.pos()[1]
+#     x_max_bound = x_min_bound + roi.size()[0]
+#     y_max_bound = y_min_bound + roi.size()[1]
+#     ## check which points are within these bounds
+#     for pts in spots:
+#         if pts['pos'][0] > x_min_bound and pts['pos'][0] < x_max_bound and pts['pos'][1] > y_min_bound and pts['pos'][1] < y_max_bound:
+#             pointCount += 1
+#             point_combined_length += pts['size']
+#     if pts > 0:
+#         print 'total points selected:', pointCount
+#         print 'length of contigs (bps):', int(point_combined_length)
+#         window2.setText('you have selected %s points' % pointCount)
+#         contig_length_view.setText('length of contigs (bps):')
+#
+# #TODO: create module to estimate genome coverage, etc.
+#
+# # this is a 'signal' in pyqtgraph used to call the function
+#
+# roi.sigRegionChangeFinished.connect(roiSelector)
 
 
 # Start Qt event loop unless running in interactive mode.
---
+++ symBinner/symbROIselector.py
@@ -0,0 +1,8 @@
+__author__ = 'neave'
+
+# calculate information from the Region of Interest (ROI selector)
+# Matthew J. Neave 2.2.15
+
+
+
+#TODO: make use of sigClicked? To give info about specific point?
