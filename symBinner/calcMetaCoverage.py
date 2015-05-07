803c403c75452e9761b14097a42a9e341e9e05c3
neavemj <matthewjneave1@gmail.com>
2015-05-07 16:34:33 +0300

    improved argparse

--- symBinner/calcMetaCoverage.py
+++ symBinner/calcMetaCoverage.py
@@ -10,17 +10,18 @@ Matthew J. Neave 7.5.15
 import argparse
 import subprocess
 import os
+import sys
 
 # use argparse to get fastq files and assembly file
 
 parser = argparse.ArgumentParser("calculate average coverage of each contig in a metagenomic assembly")
 
-parser.add_argument('fastq_files', type = str,
-                    nargs = "+", help = "forward and reverse fastq files")
+parser.add_argument('fastq_files', type=str,
+                    nargs = 2, help = "forward and reverse fastq files")
 parser.add_argument('assembly_file', type = str,
-                    nargs = "?", help = "metagenomic assembly fasta file")
+                    nargs = 1, help = "metagenomic assembly fasta file")
 parser.add_argument('--threads', '-t', type = int,
-                    nargs = "?", help = "number of threads for bowtie (default: 1)")
+                    nargs = 1, help = "number of threads for bowtie (default: 1)")
 
 args = parser.parse_args()
 
@@ -33,6 +34,13 @@ def progAvail(*args):
                 subprocess.call(prog, stdout=quiet, stderr=quiet)
         except OSError:
             print "couldn't find %s in your path" % prog
+            sys.exit(1)
 
 progAvail("bowtie2", "bedtools")
 
+# create bowtie2 reference of the assembly
+
+if args.assembly_file:
+    print "you have a file!"
+else:
+    print "metagenomic assembly file not given"
